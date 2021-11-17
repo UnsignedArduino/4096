4b7e7bb2f94e04b84e6e00d1ec0305b255d359d4
Ckyiu <UnsignedArduino@outlook.com>
2021-11-17 12:34:40 -0500

    Make tilemap

--- main.ts
+++ main.ts
@@ -1,1 +1,7 @@
-
+function prepare_tilemap () {
+    scene.setBackgroundColor(13)
+    tiles.loadMap(tiles.createMap(tilemap`screen`))
+    scene.centerCameraAt(scene.cameraProperty(CameraProperty.X), scene.cameraProperty(CameraProperty.Y) + tiles.tileWidth() / 4)
+}
+stats.turnStats(true)
+prepare_tilemap()
