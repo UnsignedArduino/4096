0f37d3236f26b46c8735f9b7e1dd77ad78e03177
Ckyiu <UnsignedArduino@outlook.com>
2021-11-17 12:55:00 -0500

    Can summon tiles

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "tile":return img`
+. c c c c c c c c c c c c c c . 
+c c d d d d d d d d d d d d c c 
+c d d d d d d d d d d d d d d c 
+c d d d d d d d d d d d d d d c 
+c d d d d d d d d d d d d d d c 
+c d d d d d d d d d d d d d d c 
+c d d d d d d d d d d d d d d c 
+c d d d d d d d d d d d d d d c 
+c d d d d d d d d d d d d d d c 
+c d d d d d d d d d d d d d d c 
+c d d d d d d d d d d d d d d c 
+c d d d d d d d d d d d d d d c 
+c d d d d d d d d d d d d d d c 
+c d d d d d d d d d d d d d d c 
+c c d d d d d d d d d d d d c c 
+. c c c c c c c c c c c c c c . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -3,5 +3,29 @@ function prepare_tilemap () {
     tiles.loadMap(tiles.createMap(tilemap`screen`))
     scene.centerCameraAt(scene.cameraProperty(CameraProperty.X), scene.cameraProperty(CameraProperty.Y) + tiles.tileWidth() / 4)
 }
+function get_empty_spot () {
+    location = tiles.getTileLocation(randint(1, 6), randint(1, 6))
+    while (grid.getSprites(location).length > 0) {
+        location = tiles.getTileLocation(randint(1, 6), randint(1, 6))
+    }
+    return location
+}
+function has_empty_spot () {
+    for (let index = 0; index <= 5; index++) {
+        if (grid.rowSprites(index + 1).length < 6) {
+            return true
+        }
+    }
+    return false
+}
+function add_number (num: number) {
+    tile = sprites.create(assets.image`tile`, SpriteKind.Player)
+    grid.place(tile, get_empty_spot())
+    images.printCenter(tile.image, convertToText(num), 4, 15)
+    sprites.setDataNumber(tile, "value", num)
+}
+let tile: Sprite = null
+let location: tiles.Location = null
 stats.turnStats(true)
 prepare_tilemap()
+add_number(2)
