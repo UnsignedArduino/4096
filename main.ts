efeb274fb8f2fbc9e3129ad52f17639cc99509a8
Ckyiu <UnsignedArduino@outlook.com>
2021-11-18 09:03:55 -0500

    Can print numbers to blocks better and auto spawn

--- main.ts
+++ main.ts
@@ -21,11 +21,46 @@ function has_empty_spot () {
 function add_number (num: number) {
     tile = sprites.create(assets.image`tile`, SpriteKind.Player)
     grid.place(tile, get_empty_spot())
-    images.printCenter(tile.image, convertToText(num), 4, 15)
     sprites.setDataNumber(tile, "value", num)
+    print_num(tile.image, num)
 }
+function print_num (image_ptr: Image, num: number) {
+    if (!(text_sprite)) {
+        text_sprite = textsprite.create("", 0, 15)
+        text_sprite.setMaxFontHeight(5)
+        text_sprite.setFlag(SpriteFlag.Invisible, true)
+    }
+    num_as_str = convertToText(num)
+    text_sprite.setText(num_as_str)
+    if (num < 10) {
+        spriteutils.drawTransparentImage(text_sprite.image, image_ptr, 6, 6)
+    } else if (num < 100) {
+        spriteutils.drawTransparentImage(text_sprite.image, image_ptr, 3, 5)
+    } else {
+        num_y = 2
+        for (let index = 0; index <= (convertToText(num).length - 1) / 2; index++) {
+            num_as_str = "" + convertToText(num).charAt(index * 2) + convertToText(num).charAt(index * 2 + 1)
+            text_sprite.setText(num_as_str)
+            spriteutils.drawTransparentImage(text_sprite.image, image_ptr, 3, num_y)
+            num_y += 6
+        }
+    }
+}
+let num_y = 0
+let num_as_str = ""
+let text_sprite: TextSprite = null
 let tile: Sprite = null
 let location: tiles.Location = null
 stats.turnStats(true)
 prepare_tilemap()
-add_number(2)
+for (let index = 0; index < 2; index++) {
+    add_number(2)
+}
+forever(function () {
+    if (has_empty_spot()) {
+        add_number(2)
+    } else {
+        game.over(false)
+    }
+    pause(2000)
+})
